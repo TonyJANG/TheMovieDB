71bacd9c50fc1e5cb8de47921f3514597407e8fa
Juan Antonio Naranjo Garcia <j.naranjo@CO-IT01007.local>
2017-09-06 11:47:52 -0500

    testingfggggg

---
+++ TheMovieDB/TheMovieDB/Protocol.swift
@@ -0,0 +1,9 @@
+//
+//  Protocol.swift
+//  TheMovieDB
+//
+//  Created by Juan Antonio Naranjo Garcia on 9/5/17.
+//  Copyright © 2017 Globant. All rights reserved.
+//
+
+import Foundation
---
+++ TheMovieDB/TheMovieDB/RequestFacade.swift
@@ -0,0 +1,9 @@
+//
+//  RequestFacade.swift
+//  TheMovieDB
+//
+//  Created by Juan Antonio Naranjo Garcia on 9/5/17.
+//  Copyright © 2017 Globant. All rights reserved.
+//
+
+import Foundation
